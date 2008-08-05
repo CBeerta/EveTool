0027bcd507f5c301b629bd57ed3177d39a53bf45
Claus Beerta <claus@beerta.de>
2008-08-05 05:52:28 +0000

    git-svn-id: svn://localhost/evetool/trunk@406 0a101332-4a5c-4009-ba00-5ad6272eac1f

--- application/views/todolist.php
+++ application/views/todolist.php
@@ -5,16 +5,16 @@ FIXME:
  - Handle change of ApiKeys
  - Move character list to database, and maybe out of the Controller
  - Allow Users to change their Passwords
- - Allow for Password recovery
+ - DONE: Allow for Password recovery
  - Better Handling for eve-o downtimes
     
 FEATURES:
  - Make Assets searchable (Maybe move them to the Database for that)
  - Complete Production Page 
- - Image Caching
- - Portrait Caching
+ - DONE: Image Caching
+ - DONE: Portrait Caching
  - Mineral Calculator
- - Skill View ala newforms
+ - DONE: Skill View ala newforms
 
 FAR OUT FEATURES:
  - POS Stuff, need to set me one up first
