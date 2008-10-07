f0e9fb1a73966715e9a001448bcb53d008289d06
Claus Beerta <claus@beerta.de>
2008-10-07 15:31:47 +0000

    git-svn-id: svn://localhost/evetool/trunk@425 0a101332-4a5c-4009-ba00-5ad6272eac1f

---
+++ lib/eveapi/eveapi/class.industryjobs.php
@@ -0,0 +1,49 @@
+<?php
+/**************************************************************************
+	PHP Api Lib IndustryJobs Class
+	Copyright (c) 2008 Dustin Tinklin
+
+	This file is part of PHP Api Lib.
+
+	PHP Api Lib is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	PHP Api Lib is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public License
+	along with PHP Api Lib.  If not, see <http://www.gnu.org/licenses/>.
+**************************************************************************/
+
+class IndustryJobs
+{
+	function getIndustryJobs($contents)
+	{
+	if (!empty($contents) && is_string($contents))
+		{
+	        $output = array();
+	 		$xml = new SimpleXMLElement($contents);
+
+	        // add all jobs in an array
+			foreach ($xml->result->rowset->row as $row)
+			{
+				$index = count($output);
+				foreach ($row->attributes() as $name => $value)
+				{
+				$output[$index][(string) $name] = (string) $value;
+				}
+			}
+			unset ($xml); // manual garbage collection
+			return $output;
+		}
+	else
+		{
+			return null;
+		}
+	}
+}
+?>
---
+++ lib/eveapi/sample/config.php
@@ -0,0 +1,6 @@
+<?php
+// Hard-code your API user and key as well as character name - for the api-testing code to work, this will have to be your full key
+$apiuser = 'your-user-id-here';
+$apipass = 'your-full-key-here';
+$mychar = 'your-character-name-here';
+?>
