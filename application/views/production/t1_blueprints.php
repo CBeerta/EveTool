38945c46e3fcb18ab9c3463757ffec7e92a3ed5a
Claus Beerta <claus@beerta.de>
2008-11-29 09:09:06 +0000

    Drones in t1 production added
    git-svn-id: svn://localhost/evetool/trunk@435 0a101332-4a5c-4009-ba00-5ad6272eac1f

--- application/views/production/t1_blueprints.php
+++ application/views/production/t1_blueprints.php
@@ -26,5 +26,19 @@
     </tr>
 <?php endforeach;?>
 </tr>
+<table width="100%">
+<caption>Drones</caption>
+<tr>
+<?php foreach ($t1[18] as $k => $v): ?>
+    <tr>
+        <?php echo form_open('production/t1/detail/'.$character, array('name' => 'form'.str_replace(' ', '_', $k))); ?>
+        <th><?php echo $k; ?></th>
+        <td style="text-align: left;"><?php echo form_dropdown($k, $v); ?></td>
+        <td><?php echo form_submit('submit', 'Go'); ?></td>
+        <?php echo form_close(); ?>
+    </tr>
+<?php endforeach;?>
+</tr>
+
 </table>
 
