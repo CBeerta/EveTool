cf26b329766f9ab23a225766dc254adf2d421a37
Claus Beerta <claus@beerta.de>
2008-12-12 21:03:23 +0000

    pretty daily wallet journal, stolen from ooe
    git-svn-id: svn://localhost/evetool/trunk@448 0a101332-4a5c-4009-ba00-5ad6272eac1f

--- files/eve.js
+++ files/eve.js
@@ -1,5 +1,18 @@
+function round ( val, precision ) {
+    // http://kevin.vanzonneveld.net
+    // +   original by: Philip Peterson
+    // +    revised by: Onno Marsman
+    // *     example 1: round(1241757, -3);
+    // *     returns 1: 1242000
+    // *     example 2: round(3.6);
+    // *     returns 2: 4
+
+    return parseFloat(parseFloat(val).toFixed(precision));
+}
+
 function numberFormat(nStr) 
 {
+    nStr = round(nStr, 2);
 	nStr += '';
 	x = nStr.split('.');
 	x1 = x[0];
