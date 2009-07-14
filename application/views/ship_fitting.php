135276ef47e8f8b8b6616fd2de53c0fa8234a0a0
Claus Beerta <claus@beerta.de>
2009-07-14 06:44:04 +0000

    * Added images from Killboard required for fitting display     * first basic fitting view (doesnt do techlevel and scripts yet)
    git-svn-id: svn://localhost/evetool/trunk@458 0a101332-4a5c-4009-ba00-5ad6272eac1f

---
+++ application/views/ship_fitting.php
@@ -0,0 +1,166 @@
+<div id="fitting" style="position:relative; height:398px; width:398px; background-image:url(<?php echo site_url('/files/images/panel/Windowed.png'); ?>)" name="fitting">
+    <div id="high0" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['hiSlots'].'h.gif'); ?>" border="0">
+     </div>
+    <div id="highc" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['hiSlots'].'ch.gif'); ?>" border="0">
+    </div>
+    <div id="mid0" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['medSlots'].'m.gif'); ?>" border="0">
+    </div>
+    <div id="midx" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['medSlots'].'cm.gif'); ?>" border="0">
+    </div>
+    <div id="low0" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['lowSlots'].'l.gif'); ?>" border="0">
+    </div>
+    <div id="lowx" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['lowSlots'].'cl.gif'); ?>" border="0">
+    </div>
+    <div id="rig0" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['rigSlots'].'r.gif'); ?>" border="0">
+    </div>
+    
+    <!--
+    <div id="mask" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img border="0" style="position:absolute; height="398" width="398" src="<?php echo site_url('/files/images/panel/CoolGrey.png'); ?>">
+    </div>
+    -->
+    
+    <div id="highx" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['hiSlots'].'ph.gif'); ?>" border="0">
+    </div>
+    <!--
+        ############### HIGH SLOTS ####################
+    -->
+    <div id="high1" style="position:absolute; left:40px; top:278px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.0.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.0.Alt']; ?>" title="<?php echo $fitting['high.0.Alt']; ?>">
+    </div>
+    <div id="high1a" style="position:absolute; left:63px; top:304px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.0.show']; ?></div>
+    <div id="high1l" style="position:absolute; left:67px; top:308px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.0.type']; ?></div>
+    <div id="high2" style="position:absolute; left:9px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.1.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.1.Alt']; ?>" title="<?php echo $fitting['high.1.Alt']; ?>">
+    </div>
+    <div id="high2a" style="position:absolute; left:18px; top:209px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.1.show']; ?></div>
+    <div id="high2l" style="position:absolute; left:22px; top:213px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.1.type']; ?></div>
+    <div id="high3" style="position:absolute; left:40px; top:83px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.2.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.2.Alt']; ?>" title="<?php echo $fitting['high.2.Alt']; ?>">
+    </div>
+    <div id="high3a" style="position:absolute; left:36px; top:109px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.2.show']; ?></div>
+    <div id="high3l" style="position:absolute; left:40px; top:113px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.2.type']; ?></div>
+    <div id="high4" style="position:absolute; left:124px; top:22px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.3.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.3.Alt']; ?>" title="<?php echo $fitting['high.3.Alt']; ?>">
+    </div>
+    <div id="high4a" style="position:absolute; left:113px; top:38px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.3.show']; ?></div>
+    <div id="high4l" style="position:absolute; left:117px; top:42px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.3.type']; ?></div>
+    <div id="high5" style="position:absolute; left:227px; top:22px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.4.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.4.Alt']; ?>" title="<?php echo $fitting['high.4.Alt']; ?>">
+    </div>
+    <div id="high5a" style="position:absolute; left:255px; top:38px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.4.show']; ?></div>
+    <div id="high5l" style="position:absolute; left:259px; top:42px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.4.type']; ?></div>
+    <div id="high6" style="position:absolute; left:310px; top:83px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.5.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.5.Alt']; ?>" title="<?php echo $fitting['high.5.Alt']; ?>">
+    </div>
+    <div id="high6a" style="position:absolute; left:330px; top:110px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.5.show']; ?></div>
+    <div id="high6l" style="position:absolute; left:334px; top:114px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.5.type']; ?></div>
+    <div id="high7" style="position:absolute; left:342px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.6.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.6.Alt']; ?>" title="<?php echo $fitting['high.6.Alt']; ?>">
+    </div>
+    <div id="high7a" style="position:absolute; left:348px; top:210px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.6.show']; ?></div>
+    <div id="high7l" style="position:absolute; left:352px; top:214px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.6.type']; ?></div>
+    <div id="high8" style="position:absolute; left:310px; top:278px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['high.7.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['high.7.Alt']; ?>" title="<?php echo $fitting['high.7.Alt']; ?>">
+    </div> 
+    <div id="high8a" style="position:absolute; left:305px; top:302px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_high.7.show']; ?></div>
+    <div id="high8l" style="position:absolute; left:309px; top:306px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_high.7.type']; ?></div>
+    
+    <!--
+        ############### MED SLOTS ####################
+    -->
+    <div id="midx" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['medSlots'].'pm.gif'); ?>" border="0">
+    </div>
+    <div id="mid1" style="position:absolute; left:76px; top:253px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.0.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.0.Alt']; ?>" title="<?php echo $fitting['med.0.Alt']; ?>">
+    </div>
+    <div id="mid1a" style="position:absolute; left:100px; top:277px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.0.show']; ?></div>
+    <div id="mid1l" style="position:absolute; left:104px; top:281px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.0.type']; ?></div>
+    <div id="mid2" style="position:absolute; left:52px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.1.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.1.Alt']; ?>" title="<?php echo $fitting['med.1.Alt']; ?>">
+    </div>
+    <div id="mid2a" style="position:absolute; left:65px; top:210px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.1.show']; ?></div>
+    <div id="mid2l" style="position:absolute; left:69px; top:214px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.1.type']; ?></div>
+    <div id="mid3" style="position:absolute; left:77px; top:108px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.2.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.2.Alt']; ?>" title="<?php echo $fitting['med.2.Alt']; ?>">
+    </div>
+    <div id="mid3a" style="position:absolute; left:74px; top:136px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.2.show']; ?></div>
+    <div id="mid4l" style="position:absolute; left:78px; top:140px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.2.type']; ?></div>
+    <div id="mid4" style="position:absolute; left:138px; top:66px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.3.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.3.Alt']; ?>" title="<?php echo $fitting['med.3.Alt']; ?>">
+    </div>
+    <div id="mid4a" style="position:absolute; left:124px; top:81px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.3.show']; ?></div>
+    <div id="mid4l" style="position:absolute; left:128px; top:85px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.3.type']; ?></div>
+    <div id="mid5" style="position:absolute; left:213px; top:66px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.4.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.4.Alt']; ?>" title="<?php echo $fitting['med.4.Alt']; ?>">
+    </div>
+    <div id="mid5a" style="position:absolute; left:241px; top:81px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.4.show']; ?></div>
+    <div id="mid5l" style="position:absolute; left:245px; top:85px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.4.type']; ?></div>
+    <div id="mid6" style="position:absolute; left:274px; top:108px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.5.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.5.Alt']; ?>" title="<?php echo $fitting['med.5.Alt']; ?>">
+    </div>
+    <div id="mid6a" style="position:absolute; left:292px; top:137px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.5.show']; ?></div>
+    <div id="mid6l" style="position:absolute; left:296px; top:141px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.5.type']; ?></div>
+    <div id="mid7" style="position:absolute; left:298px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.6.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.6.Alt']; ?>" title="<?php echo $fitting['med.6.Alt']; ?>">
+    </div>
+    <div id="mid7a" style="position:absolute; left:302px; top:210px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.6.show']; ?></div>
+    <div id="mid7l" style="position:absolute; left:306px; top:214px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.6.type']; ?></div>
+    <div id="mid8" style="position:absolute; left:275px; top:253px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['med.7.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['med.7.Alt']; ?>" title="<?php echo $fitting['med.7.Alt']; ?>">
+    </div> 
+    <div id="mid8a" style="position:absolute; left:267px; top:276px; width:32px; height:32px; z-index:2;"><?php echo $fitting['ammo_mid.7.show']; ?></div>
+    <div id="mid8l" style="position:absolute; left:271px; top:280px; width:24px; height:24px; z-index:2;"><?php echo $fitting['ammo_mid.7.type']; ?></div>
+
+    <!--
+        ############### LOW SLOTS ####################
+    -->
+    <div id="lowx" style="position:absolute; left:0px; top:0px; width:398px; height:398px; z-index:0;">
+            <img src="<?php echo site_url('/files/images/panel/'.$slots['lowSlots'].'pl.gif'); ?>" border="0">
+    </div>
+    <div id="low1" style="position:absolute; left:114px; top:226px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.0.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.0.Alt']; ?>" title="<?php echo $fitting['low.0.Alt']; ?>">
+    </div>
+    <div id="low2" style="position:absolute; left:98px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.1.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.1.Alt']; ?>" title="<?php echo $fitting['low.1.Alt']; ?>">
+    </div>
+    <div id="low3" style="position:absolute; left:114px; top:135px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.2.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.2.Alt']; ?>" title="<?php echo $fitting['low.2.Alt']; ?>">
+    </div>
+    <div id="low4" style="position:absolute; left:151px; top:110px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.3.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.3.Alt']; ?>" title="<?php echo $fitting['low.3.Alt']; ?>">
+    </div>
+    <div id="low5" style="position:absolute; left:198px; top:110px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.4.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.4.Alt']; ?>" title="<?php echo $fitting['low.4.Alt']; ?>">
+    </div>
+    <div id="low6" style="position:absolute; left:236px; top:135px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.5.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.5.Alt']; ?>" title="<?php echo $fitting['low.5.Alt']; ?>">
+    </div>
+    <div id="low7" style="position:absolute; left:250px; top:180px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.6.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.6.Alt']; ?>" title="<?php echo $fitting['low.6.Alt']; ?>">
+    </div>
+    <div id="low8" style="position:absolute; left:236px; top:226px; width:48px; height:48px; z-index:1;">
+        <img src="<?php echo $fitting['low.7.Icon']; ?>" height="48" width="48" border="0" alt="<?php echo $fitting['low.7.Alt']; ?>" title="<?php echo $fitting['low.7.Alt']; ?>">
+    </div>
+
+    <div id="rig1" style="position:absolute; left:66px; top:355px; width:32px; height:32px; z-index:1;">
+        <img src="<?php echo $fitting['rig.0.Icon']; ?>" height="32" width="32" border="0">
+    </div>
+    <div id="rig2" style="position:absolute; left:100px; top:355px; width:32px; height:32px; z-index:1;">
+        <img src="<?php echo $fitting['rig.1.Icon']; ?>" height="32" width="32" border="0">
+    </div>
+    <div id="rig3" style="position:absolute; left:134px; top:355px; width:32px; height:32px; z-index:1;">
+        <img src="<?php echo $fitting['rig.2.Icon']; ?>" height="32" width="32" border="0">
+    </div>
+
+</div>
+
